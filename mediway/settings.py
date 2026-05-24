5626a3026926314d1f1ed7309009b1ba320f91d7
Sunil A. Bankar <sunilabankar@email.com>
2026-05-24 11:42:25 +0530

    Enable debug mode

--- mediway/settings.py
+++ mediway/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-($)4@p9dbe-bi)&+1mwj#!90b(_9dsd*uzt9821&n4ul^2#v8l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
